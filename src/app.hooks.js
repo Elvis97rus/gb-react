8f1ade7ac1d857798b73e01e660fc01560fb4efb
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-08-06 15:41:59 +0300

    lesson2 - hw2 hooks, component's life cycle

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,16 +1,29 @@
-import {useState, useEffect, useCallback, useMemo, useLayoutEffect, useReducer} from "react"
+import {useState, useEffect, useCallback, useMemo, useLayoutEffect, useReducer, memo} from "react"
 
-export const AppHooks = () => {
-    const [counter, setCounter] = useState(1)
+const useMyHook = () => {
     const [messages, setMessages] = useState([
         //{value: 'Hello', author: "Vasya"},
     ])
     const [value, setValue] = useState("")
 
+    return {messages, setMessages, value, setValue}
+}
+
+export const AppHooks = () => {
+    const [counter, setCounter] = useState(1)
+
+    const {messages, setMessages,value, setValue} = useMyHook()
+
     const handleSendMessage = () => {
         setMessages(state => [...state, {value, author: "Vasya"}])
         setValue("")
     }
+
+    const foo = useCallback(() => {}, [])
+    const sum = useMemo(() => {
+        console.log("memo")
+        return 1+1
+    }, [])
 
     useEffect(() =>{
         if (messages[messages.length-1] &&messages[messages.length-1].author !== "System"){
@@ -19,6 +32,10 @@ export const AppHooks = () => {
             },1500)
         }
     }, [messages])
+
+    useLayoutEffect(()=>{
+
+    },[])
 
     return (
         <div>
@@ -34,6 +51,13 @@ export const AppHooks = () => {
             </ul>
             <input value={value} onChange={(e) => setValue(e.target.value)} />
             <button onClick={handleSendMessage}>send</button>
+
+            <Test foo={foo} />
         </div>
     )
 }
+
+const Test = memo(() => {
+    console.log("render")
+    return <h1>Test__</h1>
+})
