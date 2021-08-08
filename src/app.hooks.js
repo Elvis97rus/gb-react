2491f0cc6facb0fbd137da9c2c1aae1360898223
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-08-08 07:10:58 +0300

    lesson3 - hw2(cleaned) hooks, component's life cycle

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,20 +1,68 @@
-import {useState, useEffect, useCallback, useMemo, useLayoutEffect, useReducer, memo} from "react"
+import {useState, useEffect, useRef} from "react"
+import {Button, TextareaAutosize, Grid, makeStyles, InputBase, ListItem, List} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => {
+    return {
+        root: {
+            flexGrow: 1,
+            background: theme.light.color,
+        },
+        system: {
+            float:"left",
+            background: "#b3abab",
+        },
+        user: {
+            background: "#708fe8",
+            float:"right",
+        },
+        sendMsgBtn: {
+            width: "100%"
+        }
+    }
+})
+
+export const ChatList = () => {
+    const [chats, setChats] = useState([{value: "Current chat - 1", id: 'first-chat-unique-phrase'}])
+    return (
+        <>
+        <div>ChatList</div>
+            <List component="nav" aria-label="contacts">
+                {chats.map((chat, id) => (
+                    <ListItem button>
+                        <p key={id}><strong>{chat.value}</strong>: ({chat.id})</p>
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    )
+}
 
 export const AppHooks = () => {
+    const ref = useRef(null)
+
+    const classes = useStyles()
     const [messages, setMessages] = useState([])
 
     const [value, setValue] = useState("")
 
     const handleSendMessage = () => {
-        setMessages(state => [...state, {value, author: "Vasya"}])
+        setMessages(state => [...state, {value, author: "User"}])
         setValue("")
     }
 
-    useEffect(() =>{
-        if (messages[messages.length-1] &&messages[messages.length-1].author !== "System"){
-            setTimeout(()=>{
-                setMessages(state => [...state, {value:"Message received", author: "System"}])
-            },1500)
+    const handleEnterMessage = ({ code }) => {
+        if (code === "Enter") {
+            handleSendMessage()
+        }
+    }
+
+    useEffect(() => {
+        ref.current.focus()
+        if (messages[messages.length - 1] && messages[messages.length - 1].author !== "System") {
+            setTimeout(() => {
+                setMessages(state => [...state, {value: "Message received", author: "System"}])
+            }, 1500)
+
         }
     }, [messages])
 
@@ -23,13 +71,32 @@ export const AppHooks = () => {
         <div>
             <h1>AppHooks - Messages_HW</h1>
 
-            <ul>
-                {messages.map((message, id) => (
-                    <li key={id}> {message.value} = {message.author}</li>
-                ))}
-            </ul>
-            <input value={value} onChange={(e) => setValue(e.target.value)} />
-            <button onClick={handleSendMessage}>send</button>
+            <div className={classes.root}>
+                <Grid container={true} spacing={3}>
+                    <Grid item={true} xs={6}>
+                        <ChatList />
+                    </Grid>
+                    <Grid item={true} xs={6}>
+                        <Grid container={true}
+                              direction="column"
+                              justifyContent="flex-end"
+                              alignItems="stretch">
+                            {messages.map((message, id) => (
+                                <Grid item={true} xs={12}>
+                                    {(message.author==="System")?
+                                        <span key={id} className={classes.system}>{message.value} = {message.author}</span>:
+                                        <span key={id} className={classes.user}>{message.value} = {message.author}</span>}
+
+                                </Grid>
+                            ))}
+                        </Grid>
+
+                        <InputBase inputRef={ref} fullWidth={true} onKeyPress={handleEnterMessage} value={value} onChange={(e) => setValue(e.target.value)}/>
+                        <Button className={classes.sendMsgBtn} variant="contained" color="primary" onClick={handleSendMessage}>send</Button>
+                    </Grid>
+                </Grid>
+            </div>
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
 import {AppHooks} from "./app.hooks";
+import { ThemeProvider, createTheme } from "@material-ui/core"
 
-class Test extends React.Component {
-    state = {
-        isVisible: true,
-    }
-
-    toggle = () => {
-        this.setState({ isVisible: !this.state.isVisible })
-    }
-
-    render() {
-        const { isVisible } = this.state
-
-        return (
-            <div>
-                <button onClick={this.toggle}>toggle</button>
-                {isVisible && <App toggle={this.toggle} />}
-            </div>
-        )
-    }
-}
+const theme = createTheme({
+    dark: {
+        color: "#000",
+    },
+    light: {
+        color: "lightgray",
+    },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppHooks />
+      <ThemeProvider theme={theme}>
+            <AppHooks />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
