38cd71a47fdf7d008807a5e6f63b2abf9da7cb9e
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-08-16 13:34:40 +0300

    lesson4 - hw4 RMK (upd)

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,56 +1,57 @@
-import { useEffect } from "react"
-import { Switch, Route, useHistory } from "react-router-dom"
+import {useEffect} from "react"
+import {Switch, Route, useHistory} from "react-router-dom"
 import {
-  MessageProvider,
-  Layout,
-  Header,
-  ChatList,
-  MessageList,
+    MessageProvider,
+    Layout,
+    Header,
+    ChatList,
+    MessageList,
 } from "../components"
+import styles from "../components/chat-list/chat/chat.module.css"
 
 export function Chat() {
-  const { push } = useHistory()
+    const {push} = useHistory()
 
-  useEffect(() => {
-    const listenExistChat = ({ code }) => {
-      if (code === "Escape") {
-        push("/chat")
-      }
-    }
+    useEffect(() => {
+        const listenExistChat = ({code}) => {
+            if (code === "Escape") {
+                push("/chat")
+            }
+        }
 
-    document.addEventListener("keydown", listenExistChat)
+        document.addEventListener("keydown", listenExistChat)
 
-    return () => {
-      document.removeEventListener("keydown", listenExistChat)
-    }
-  }, [push])
+        return () => {
+            document.removeEventListener("keydown", listenExistChat)
+        }
+    }, [push])
 
-  return (
-    <Switch>
-      <Route path={["/chat/:roomId", "/chat"]}>
-        <MessageProvider>
-          {([state, actions]) => (
-            <Layout header={<Header />} chats={<ChatList {...state} />}>
-              {state.hasRoomById ? (
-                  <Route path="/chat/:roomId">
-                    <MessageList {...state} {...actions}/>
-                  </Route>
-              ) : (
-                  <Route path="/chat/:roomId">
-                    <h1>такого чата нет</h1>
-                  </Route>
-              )}
-              <Route exact={true} path="/chat">
-                <h1>выберите сообщение</h1>
-              </Route>
-            </Layout>
-          )}
-        </MessageProvider>
-      </Route>
+    return (
+        <Switch>
+            <Route path={["/chat/:roomId", "/chat"]}>
+                <MessageProvider>
+                    {([state, actions]) => (
+                        <Layout header={<Header/>} chats={<ChatList {...state} />}>
+                            {state.hasRoomById ? (
+                                <Route path="/chat/:roomId">
+                                    <MessageList {...state} {...actions}/>
+                                </Route>
+                            ) : (
+                                <Route path="/chat/:roomId">
+                                    <h1 className={styles.notification}>такого чата нет</h1>
+                                </Route>
+                            )}
+                            <Route exact={true} path="/chat">
+                                <h1 className={styles.notification}>выберите сообщение</h1>
+                            </Route>
+                        </Layout>
+                    )}
+                </MessageProvider>
+            </Route>
 
-      <Route path="*">
-        <h1>такого чата нет</h1>
-      </Route>
-    </Switch>
-  )
+            <Route path="*">
+                <h1 className={styles.notification}>такого чата нет</h1>
+            </Route>
+        </Switch>
+    )
 }
